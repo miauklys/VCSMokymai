d423c3f11aaac2c682f759772e93426e35ede6f3
Andrius Sakavicius <sakavicius.andrius@gmail.com>
2021-10-18 20:18:51 +0300

    Ketvirtoji uzduotis

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskFourClass prog = new TaskFourClass();
+
+        }
+    }
+}
