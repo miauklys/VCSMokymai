d3fd75d260593f1db79f74e3bddb9e77335a448d
Andrius Sakavicius <sakavicius.andrius@gmail.com>
2021-10-18 20:18:21 +0300

    Antroji uzduotis

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Skaicius kazkoks = new Skaicius();
+            //kazkoks.ivestis();
+            //string ivestis = Console.ReadLine();
+
+            //List<Skaicius> skaiciukai = new List<Skaicius>();
+            //skaiciukai.Add(kazkoks);
+            //kazkoks.ivestis();
+            //Console.ReadLine();
+
+            Skaicius kazkoksKitas = new Skaicius();
+
+            int sk = kazkoksKitas.suma();
+            Console.ReadLine();
+
+        }
+    }
+}
