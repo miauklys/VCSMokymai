aa5b81478bc34565814ecfa66f1340e646e2d789
Andrius Sakavicius <sakavicius.andrius@gmail.com>
2021-10-27 16:04:28 +0300

    Pataisytas kodas.

---
+++ Task3/IvedimoIrIsvedimoKlase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class IvedimoIrIsvedimoKlase
+    {
+        public List<int> IvestiSkaicius() //tipas turi buti List
+        {
+            List<int> skaiciusSarasas = new List<int>();
+            Console.WriteLine("Įveskite skaičių:");
+            int ivestasSkaicius = Convert.ToInt32(Console.ReadLine());
+
+            if (ivestasSkaicius != 0)
+            {
+                skaiciusSarasas.Add(ivestasSkaicius);
+                Console.WriteLine("Įveskite skaičių:");
+                ivestasSkaicius = Convert.ToInt32(Console.ReadLine());
+                skaiciusSarasas.Add(ivestasSkaicius);
+
+                while (ivestasSkaicius != 0)
+                {
+                    Console.WriteLine("Įveskite skaičių:");
+                    ivestasSkaicius = Convert.ToInt32(Console.ReadLine());
+                    skaiciusSarasas.Add(ivestasSkaicius);
+                }
+            }
+            return skaiciusSarasas;
+        }
+
+        public void AtspausdintiSkaicius(List<int> sarasas)
+        {
+            foreach (int skaiciusSarase in sarasas)
+            {
+                Console.Write(skaiciusSarase);
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IvedimoIrIsvedimoKlase ivedimoPrograma = new IvedimoIrIsvedimoKlase();
+            List<int> sudarytasSarasas = ivedimoPrograma.IvestiSkaicius(); //įvesti duomenys išsaugojami sąraše
+
+            IvedimoIrIsvedimoKlase isvedimoPrograma = new IvedimoIrIsvedimoKlase();
+            isvedimoPrograma.AtspausdintiSkaicius(sudarytasSarasas);
+
+            Console.ReadLine();
+        }
+    }
+}
