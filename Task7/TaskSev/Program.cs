1e94a87a5a0a0c0f298be44134ae54853378839d
Andrius Sakavicius <sakavicius.andrius@gmail.com>
2021-11-04 16:11:41 +0200

    Septinta užduotis.

---
+++ Task7/TaskSev/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TaskSev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir = $"C:/Users/sakav/user.txt";
+
+            User logger = new User();
+            string ivestis = logger.Logger();
+            Selection pas = logger.LoggingIn(ivestis);
+            Prisijungimas(pas);
+
+            Console.ReadLine();
+        }
+        private static string Prisijungimas(Selection pas)
+        {
+
+            switch (pas)
+            {
+                case Selection.LogIn:
+                    User p = new User(); p.LoggingIn(string kas);
+                    return string.Empty;
+                case Selection.LogOn:
+                    User pp = new User(); pp.LoggingIn(string kass);
+                    return string.Empty;
+            }
+        }
+    }
+}
